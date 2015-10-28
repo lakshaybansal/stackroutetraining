8dd57c343071a46f19eb312cc4f90f8331ca847c
Mukund Krishnamurthy <mukund.kri@gmail.com>
2015-10-28 16:01:26 +0530

    Tish message is in the vi editor

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-class ab {
-public static void main(String[] args) {
-int a=0;
-System.out.println("Hello this is my first java file in this repository");
-}
+class Ab {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println("Hello this is my first java file in this repository");
+    }
 }
